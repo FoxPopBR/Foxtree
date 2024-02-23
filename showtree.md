41b776bb5f46cc62af30d41ca9725959687af7b6
foxpopbr <fabio.towork@gmail.com>
2024-02-23 20:58:02 -0300

    Foxtree v1.0.0

---
+++ showtree.md
@@ -0,0 +1,9 @@
+<pre>
+🗃️ foxtree/
+├── 📝 showtree.md
+├── 🐍 tree.py
+└── 🛠️ modules/
+    ├── 🐍 __init__.py
+    ├── 🐍 config.py
+    └── 🐍 treemaker.py
+</pre>
---
+++ tree.py
@@ -0,0 +1,18 @@
+# tree.py
+
+import pathlib
+from modules.treemaker import TreeGenerator
+
+def main():
+    root_dir = pathlib.Path(__file__).resolve().parent
+    generator = TreeGenerator(root_dir)
+    tree = generator.generate_tree()
+
+    with open("showtree.md", "w", encoding="utf-8") as file:
+        file.write("<pre>\n")  # Adiciona a tag de início <pre>
+        for line in tree:
+            file.write(f"{line}\n")
+        file.write("</pre>")  # Adiciona a tag de fim </pre>
+
+if __name__ == "__main__":
+    main()
